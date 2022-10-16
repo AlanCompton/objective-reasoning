7525e480fbe9c9883fbe1699b3abbec81900a3fa
AlanCompton <alan.compton@codeimmersives.com>
2022-10-16 06:27:13 -0500

    main.js committed

--- main.js
+++ main.js
@@ -2,7 +2,48 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+// isAdmin
+function isAdmin(user){
+    if(user.userRole === 'ADMIN'){
+        return true;
+    }else{
+        return false;
+    }
+    //return user.userRole === "ADMIN";
+}
 
+// getEmail
+function getEmail(user){
+    let email = (`${user.firstName}.${user.lastName}@codeimmersives.com`);
+    return email.toLowerCase();
+}
+
+// getPlaylistLength
+function getPlaylistLength(playlist){
+    return playlist.songs.length;
+}
+
+// getHardestHomework
+function getHardestHomework(homeworkArray){
+    let lowestHw = homeworkArray[0];
+
+    for(let i=0; i< homeworkArray.length ;i++){
+        if(homeworkArray[i].averageScore < lowestHw.averageScore){
+            lowestHw = homeworkArray[i];
+        }
+    }
+    return lowestHw.name;
+}
+
+// createPhonebook
+function createPhonebook(names,numbers){
+    let phonebook = {};
+
+    for(let i=0; i<names.length ;i++){
+        phonebook[names[i]] = numbers[i];
+    }
+    return phonebook;
+}
 
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
